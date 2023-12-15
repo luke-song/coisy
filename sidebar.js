8528962355bcb5ac84a947512215d888f81c8289
wooooooongs <pires11233@gmail.com>
2023-12-15 17:27:31 +0900

    :sparkles: feat: add sidebar open-close event

--- sidebar.js
+++ sidebar.js
@@ -1,1 +1,11 @@
+const headerCart = document.querySelector('.header-cart');
+const cartCloseButton = document.querySelector('#close-cart');
+const cartSideBar = document.querySelector('#cart-sidebar');
 
+headerCart.addEventListener('click', () => {
+  cartSideBar.style.display = 'block';
+});
+
+cartCloseButton.addEventListener('click', () => {
+  cartSideBar.style.display = 'none';
+});
